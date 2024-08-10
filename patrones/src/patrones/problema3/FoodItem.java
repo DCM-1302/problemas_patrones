973e3e35e2e2757fbe6f64d26bd621c194f4d3f7
DCM-1302 <124814673+DCM-1302@users.noreply.github.com>
2024-08-09 19:41:58 -0500

    Update FoodItem.java

--- patrones/src/patrones/problema3/FoodItem.java
+++ patrones/src/patrones/problema3/FoodItem.java
@@ -1,4 +1,21 @@
 package patrones.problema3;
 
-public interface FoodItem {
+public abstract class FoodItem {
+
+    public final void processOrder () {
+        takeOrder();
+        prepareOrder();
+        checkAvailability();
+        calculatePrice();
+        completeOrder();
+    }
+
+    public abstract void takeOrder ();
+    public abstract void prepareOrder ();
+    public abstract void checkAvailability ();
+    public abstract void calculatePrice ();
+    private void completeOrder () {
+        System.out.println("Order Completed.");
+    }
+
 }
