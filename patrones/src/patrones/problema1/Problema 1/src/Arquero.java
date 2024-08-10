afa3e98921022f72a289e1602c300c73c2b9a5b0
camilorafe <140459693+camilorafe@users.noreply.github.com>
2024-08-09 22:44:53 -0500

    Add files via upload

---
+++ patrones/src/patrones/problema1/Problema 1/src/Arquero.java
@@ -0,0 +1,10 @@
+class Arquero extends Heroe {
+    public Arquero(String nombre, String aspectoVisual, int experiencia, int nivel, int puntosHabilidad) {
+        super(nombre, aspectoVisual, experiencia, nivel, puntosHabilidad);
+    }
+
+    @Override
+    public void mostrarHabilidades() {
+        System.out.println("Habilidades de Arquero: Tiro con arco, Evasión rápida.");
+    }
+}
---
+++ patrones/src/patrones/problema1/Problema 1/src/Guerrero.java
@@ -0,0 +1,10 @@
+class Guerrero extends Heroe {
+    public Guerrero(String nombre, String aspectoVisual, int experiencia, int nivel, int puntosHabilidad) {
+        super(nombre, aspectoVisual, experiencia, nivel, puntosHabilidad);
+    }
+
+    @Override
+    public void mostrarHabilidades() {
+        System.out.println("Habilidades de Guerrero: Ataque poderoso, Defensa robusta.");
+    }
+}
