edfcb7606b4ef09565cce46da4412603ea7f68d2
camilorafe <140459693+camilorafe@users.noreply.github.com>
2024-08-09 22:53:45 -0500

    Add files via upload

---
+++ patrones/src/patrones/problema2/Problema 2/src/Dispositivo.java
@@ -0,0 +1,3 @@
+interface Dispositivo {
+    void actualizar(String mensaje);
+}
---
+++ patrones/src/patrones/problema2/Problema 2/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Usuario usuario = new Usuario("Juan");
+
+        Dispositivo movil1 = new DispositivoMovil("Móvil 1");
+        Dispositivo movil2 = new DispositivoMovil("Móvil 2");
+        Dispositivo tablet = new DispositivoMovil("Tablet");
+
+        usuario.añadirDispositivo(movil1);
+        usuario.añadirDispositivo(movil2);
+        usuario.añadirDispositivo(tablet);
+
+        usuario.recibirMensaje("¡Hola! ¿Cómo estás?");
+
+        usuario.eliminarDispositivo(movil2);
+
+        usuario.recibirMensaje("Nos vemos mañana.");
+    }
+}
